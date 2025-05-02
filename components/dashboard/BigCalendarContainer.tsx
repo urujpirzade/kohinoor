5d3187cae5f294b7b26ad58bd7686de5de98edb8
urujpirzade <unpirzade@gmail.com>
2025-05-02 11:36:27 +0530

    Update BigCalendarContainer.tsx

--- components/dashboard/BigCalendarContainer.tsx
+++ components/dashboard/BigCalendarContainer.tsx
@@ -5,6 +5,24 @@ import { convertBigCalendarDate } from '@/lib/validate';
 const BigCalendarContainer = async () => {
   const dataRes = await prisma.event.findMany({});
 
+   dataRes.forEach((ele) => {
+    if (ele.hallHandover) {
+      const dataCopy = { ...ele };
+      dataCopy.start_time = '06:00';
+      dataCopy.end_time = '16:00';
+      dataCopy.hall = 'secondHall';
+      dataRes.push(dataCopy);
+      const dataCopy2 = { ...ele };
+      const currentDate = new Date(dataCopy2.date);
+      currentDate.setDate(currentDate.getDate() - 1);
+      dataCopy2.date = currentDate;
+      dataCopy2.hall = 'secondHall';
+      dataCopy2.start_time = '19:00';
+      dataCopy2.end_time = '23:59';
+      dataRes.push(dataCopy2);
+    }
+  });
+  
   const schedule = dataRes.map((event: any) => ({
     title: event.event_name,
     date: event.date,
